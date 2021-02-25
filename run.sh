a4b2a3b02884f2faaaadaa0e3d6d436e1a93ff1a
fando <fandofast@gmail.com>
2021-02-25 21:14:10 +0700

    f

---
+++ run.sh
@@ -0,0 +1,4 @@
+wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
+tar -zxvf ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
+cd bin
+./ethminer -P stratum1+tcp://0x93cf305db13be695909abbf51ffb2ed678c44d21.ubunt1@eth.2miners.com:2020
