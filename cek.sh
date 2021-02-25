e9394ecc6a46a83b9f531dca61c0842d800b82af
fando <fandofast@gmail.com>
2021-02-25 23:01:08 +0700

    f

--- cek.sh
+++ cek.sh
@@ -2,7 +2,8 @@ if pgrep -x "ethminer" >/dev/null
 then
     echo "ethminer is running"
 else
-    echo "$SERVICE stopped"
+  screen -d -m -S mining ./ethminer -P stratum1+tcp://0x93cf305db13be695909abbf51ffb2ed678c44d21.ubunt1@eth.2miners.com:2020
+
     # uncomment to start nginx if stopped
     # systemctl start nginx
     # mail  
--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
 wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
 tar -zxvf ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
+
+
+cp cek.sh /usr/local/bin/
+chmod +x /usr/local/bin/cek.sh
+
+crontab cron
+
+
 cd bin
 
  
